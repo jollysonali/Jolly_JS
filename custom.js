6a4eb7b74d38773b0c99ec71d2a8678955044218
Sonali Sridhar <sonali.sridhar@gmail.com>
2011-07-23 19:10:49 -0400

    This is my js.

---
+++ custom.js
@@ -0,0 +1,42 @@
+$(function() {
+    $("a#first").click(function() {
+        alert("Hello world!");
+    });
+//	$("#orderedlist").addClass("red");
+	$("#orderedlist > li").addClass("blue");
+	$("#orderedlist").hover(function(){
+		$(this).addClass("green");
+		}, function(){
+			$(this).removeClass("green");
+		});
+     
+	   $("#orderedlist").find("li").each(function(i) {
+	     $(this).append( " BAM! " + i );
+	   });	
+
+	 // use this to reset a single form
+	   $("#reset").click(function() {
+	     $("form")[0].reset();
+	   });
+	//use this to reset a single form 
+	$("#reset").click(function(){
+		$("form").each(function(){
+			this.reset();
+		});
+	});
+
+	$("li li").css("border", "1px solid black"); 
+	$("li").not(":has(ul)").css("border", "1px solid black"); 
+	
+	$('#faq').find('dd').hide();
+	$('#faq').find('dt').click(function() {
+    	$(this).next().slideToggle();
+   	});
+	$("a").hover(function(){
+		$(this).parents("p").addClass("highlight");
+	}, function(){
+		$(this).parents("p").removeClass("highlight");
+	});		
+//Annimation! yea! Need to start this...
+   
+});
