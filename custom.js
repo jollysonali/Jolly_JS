077e34b88263b88b64b8c5fc7967029cee99c513
Sonali Sridhar <sonali.sridhar@gmail.com>
2011-08-02 13:42:42 -0400

    Added Fancybox to my Starterkit.html

--- custom.js
+++ custom.js
@@ -2,47 +2,46 @@ $(function() {
     $("a#first").click(function() {
         alert("Hello world!");
     });
-//	$("#orderedlist").addClass("red");
-	$("#orderedlist > li").addClass("blue");
-	$("#orderedlist").hover(function(){
-		$(this).addClass("green");
-		}, function(){
-			$(this).removeClass("green");
-		});
-     
-	   $("#orderedlist").find("li").each(function(i) {
-	     $(this).append( " BAM! " + i );
-	   });	
+    //	$("#orderedlist").addClass("red");
+    $("#orderedlist > li").addClass("blue");
+    $("#orderedlist").hover(function() {
+        $(this).addClass("green");
+    },
+    function() {
+        $(this).removeClass("green");
+    });
 
-	 // use this to reset a single form
-	   $("#reset").click(function() {
-	     $("form")[0].reset();
-	   });
-	//use this to reset a single form 
-	$("#reset").click(function(){
-		$("form").each(function(){
-			this.reset();
-		});
-	});
+    $("#orderedlist").find("li").each(function(i) {
+        $(this).append(" BAM! " + i);
+    });
 
-	$("li li").css("border", "1px solid black"); 
-	$("li").not(":has(ul)").css("border", "1px solid black"); 
-	
-	$('#faq').find('dd').hide();
-	$('#faq').find('dt').click(function() {
-    	$(this).next().slideToggle();
-   	});
-	$("a").hover(function(){
-		$(this).parents("p").addClass("highlight");
-	}, function(){
-		$(this).parents("p").removeClass("highlight");
-	});		
-//Annimation! yea! Need to start this...
-   // work on things. 
+    // use this to reset a single form
+    $("#reset").click(function() {
+        $("form")[0].reset();
+    });
+    //use this to reset a single form
+    $("#reset").click(function() {
+        $("form").each(function() {
+            this.reset();
+        });
+    });
 
- 	$("a").toggle(function(){
-		$(".stuff").hide('slow');
-	}. function(){
-		$(".stuff").show('fast');	
-});
+    $("li li").css("border", "1px solid black");
+    $("li").not(":has(ul)").css("border", "1px solid black");
+
+    $('#faq').find('dd').hide();
+    $('#faq').find('dt').click(function() {
+        $(this).next().slideToggle();
+    });
+    $("a").click(function() {
+        $(this).parents("p").addClass("highlight");
+    });
+    //Annimation! yea! Need to start this...
+    // work on things.
+   /* $("a").toggle(function() {
+        $(".stuff").hide('slow');
+    },
+    function() {
+        $(".stuff").show('fast');
+    }); */
 });
